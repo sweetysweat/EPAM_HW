b29939d3e743960a25e7854d3f478990947ed0b9
sweetysweat <aursegov000@gmail.com>
2021-09-06 22:28:11 +0300

    изменены названия тестов для pytest

---
+++ tests/homework1/test_1.py
@@ -0,0 +1,9 @@
+import pytest
+from homework1.task1.sample_project.calculator.calc import check_power_of_2
+
+
+@pytest.mark.parametrize("test_input, expectation", [[2, True], [1, True], [0, False]])
+def test_check_power_of_2(test_input, expectation):
+    assert check_power_of_2(test_input) == expectation
+    assert check_power_of_2(test_input) == expectation
+    assert check_power_of_2(test_input) == expectation
