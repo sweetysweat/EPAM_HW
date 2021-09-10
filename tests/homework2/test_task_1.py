9db9c33333cf6a790a77b389fb094483215f71d1
sweetysweat <aursegov000@gmail.com>
2021-09-10 20:07:49 +0300

    Исправлены тесты

--- tests/homework2/test_task_1.py
+++ tests/homework2/test_task_1.py
@@ -5,7 +5,7 @@ from homework2.task1 import count_punctuation_chars, count_non_ascii_chars
 """Test get_longest_diverse_words"""
 
 
-@pytest.mark.parametrize("test_input, expected", [['test_data',
+@pytest.mark.parametrize("test_input, expected", [['test_data.txt',
                                                    ['bedenklider', 'bedenklichen', 'bedenklileser', 'hinausführen',
                                                     'vorgebahnte', 'bedenkliauf', 'bedenklisich', 'bedenklimuß',
                                                     'hinbetrachtung', 'bedenklivielmehr']
@@ -18,7 +18,7 @@ def test_get_longest_diverse_words(test_input, expected):
 """Test get_most_common_non_ascii_char"""
 
 
-@pytest.mark.parametrize("test_input, expected", ['test_data', ['ü']])
+@pytest.mark.parametrize("test_input, expected", [['test_data.txt', 'ü']])
 def test_get_most_common_non_ascii_char(test_input, expected):
     assert get_most_common_non_ascii_char(test_input) == expected
 
@@ -26,7 +26,7 @@ def test_get_most_common_non_ascii_char(test_input, expected):
 """Test get_rarest_char"""
 
 
-@pytest.mark.parametrize("test_input, expected", [['test_data', 'z']])
+@pytest.mark.parametrize("test_input, expected", [['test_data.txt', 'z']])
 def test_get_rarest_char(test_input, expected):
     assert get_rarest_char(test_input) == expected
 
@@ -34,7 +34,7 @@ def test_get_rarest_char(test_input, expected):
 """Test count_punctuation_chars"""
 
 
-@pytest.mark.parametrize("test_input, expected", [['test_data', 7]])
+@pytest.mark.parametrize("test_input, expected", [['test_data.txt', 7]])
 def test_count_punctuation_chars(test_input, expected):
     assert count_punctuation_chars(test_input) == expected
 
@@ -42,6 +42,6 @@ def test_count_punctuation_chars(test_input, expected):
 """Test count_non_ascii_chars"""
 
 
-@pytest.mark.parametrize("test_input, expected", [['test_data', 6]])
+@pytest.mark.parametrize("test_input, expected", [['test_data.txt', 6]])
 def test_count_non_ascii_chars(test_input, expected):
     assert count_non_ascii_chars(test_input) == expected
