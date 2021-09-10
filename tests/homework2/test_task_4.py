2961e53efec14051aadf738b53ba650c0958ed7b
sweetysweat <aursegov000@gmail.com>
2021-09-10 20:02:07 +0300

    доделано 4 задание

--- tests/homework2/test_task_4.py
+++ tests/homework2/test_task_4.py
@@ -1,2 +1,9 @@
 import pytest
-# разобраться в задании
+from homework2.task4 import func
+
+
+@pytest.mark.parametrize("test_input", [[100, 200]])
+def test_cache(test_input):
+    val_1 = func(*test_input)
+    val_2 = func(*test_input)
+    assert val_1 is val_2
