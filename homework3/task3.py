886f49d5ab945343b41256d1f5d7205b1c5325a5
sweetysweat <aursegov000@gmail.com>
2021-09-11 23:50:29 +0300

    измен main.yml, добавлено 4 задание и тест.

--- homework3/task3.py
+++ homework3/task3.py
@@ -13,6 +13,7 @@ class Filter:
             if all(i(item) for i in self.functions)
         ]
 
+
 # example of usage:
 # positive_even = Filter(lambda a: a % 2 == 0, lambda a: a > 0, lambda a: isinstance(int, a)))
 # positive_even.apply(range(100)) should return only even numbers from 0 to 99
@@ -26,23 +27,24 @@ def make_filter(**keywords):
     for key, value in keywords.items():
         def keyword_filter_func(value):
             return value[key] == value
+
         filter_funcs.append(keyword_filter_func)
     return Filter(filter_funcs)
 
 
 sample_data = [
-     {
-         "name": "Bill",
-         "last_name": "Gilbert",
-         "occupation": "was here",
-         "type": "person",
-     },
-     {
-         "is_dead": True,
-         "kind": "parrot",
-         "type": "bird",
-         "name": "polly"
-     }
+    {
+        "name": "Bill",
+        "last_name": "Gilbert",
+        "occupation": "was here",
+        "type": "person",
+    },
+    {
+        "is_dead": True,
+        "kind": "parrot",
+        "type": "bird",
+        "name": "polly"
+    }
 ]
 # make_filter(name='polly', type='bird').apply(sample_data) should return only second entry from the list
-# There are multiple bugs in this code. Find them all and write tests for faulty case
+# There are multiple bugs in this code. Find them all and write tests for faulty cases.
