0de381e5f178876af190ee2da286162348e2d7de
sweetysweat <aursegov000@gmail.com>
2021-09-11 17:21:41 +0300

    исправлен тест для 1 задания

--- tests/homework3/test_task_1.py
+++ tests/homework3/test_task_1.py
@@ -5,14 +5,14 @@ from unittest.mock import patch
 from homework3.task1 import f, fun
 
 
-@pytest.mark.parametrize("test_input ,expectation", [[[2, 2], [16, 16, 16]]])
-def test_fun(test_input, expectation):
+@pytest.mark.parametrize("test_input", [[2, 2]])
+def test_fun(test_input):
     a = [fun(*test_input), fun(*test_input), fun(*test_input)]
-    assert a == expectation
+    assert a[0] is a[1]
 
 
 @patch('builtins.input', lambda: 1)
 def test_f1():
     with unittest.mock.patch('builtins.input', return_value=1):
         a = [f(), f(), f()]
-        assert a == [1, 1, 1]
+        assert a[0] is a[1]
