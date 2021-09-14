505e4e5f8c560a76302d96de6eebf49bfb7b8bc3
sweetysweat <aursegov000@gmail.com>
2021-09-14 21:44:31 +0300

    изменение теста

--- tests/homework3/test_task_1.py
+++ tests/homework3/test_task_1.py
@@ -1,12 +1,35 @@
+import unittest
 import pytest
 from unittest.mock import patch
-from homework3.task1 import f, fun
+from homework3.task1 import f, fun, cache_factory
+
+test_data_for_cache_factory = 0
+
+
+def test_cache_factory_should_return_value_required_number_of_times():
+    @cache_factory(3)
+    def inner_test_func():
+        global test_data_for_cache_factory
+        test_data_for_cache_factory += 1
+        return test_data_for_cache_factory
+
+    for i in range(4):
+        inner_test_func()
+
+        """
+        Значение глобальной переменной изменится только 1 раз, так как
+        при первом вызове функции переменная увеличится на единицу, а в последующие
+        2 раза значение возьмется из кеша. Поэтому только при четвертом вызове функции
+        Значение увеличится на 1.
+        """
+
+    assert test_data_for_cache_factory == 2
 
 
 @pytest.mark.parametrize("test_input", [[2, 2]])
 def test_fun(test_input):
     a = [fun(*test_input), fun(*test_input), fun(*test_input)]
-    assert a[0] is a[3]
+    assert a[0] is a[1] and a[0] is a[2]
 
 
 @patch('builtins.input', lambda: 1)
