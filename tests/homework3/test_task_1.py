78d7fa2afee5635917e9b7668fb6fbf9fa6bfdec
sweetysweat <aursegov000@gmail.com>
2021-09-14 21:06:38 +0300

    изменен тест

--- tests/homework3/test_task_1.py
+++ tests/homework3/test_task_1.py
@@ -1,5 +1,3 @@
-import unittest.mock
-
 import pytest
 from unittest.mock import patch
 from homework3.task1 import f, fun
@@ -8,11 +6,11 @@ from homework3.task1 import f, fun
 @pytest.mark.parametrize("test_input", [[2, 2]])
 def test_fun(test_input):
     a = [fun(*test_input), fun(*test_input), fun(*test_input)]
-    assert a[0] is a[1]
+    assert a[0] is a[3]
 
 
 @patch('builtins.input', lambda: 1)
 def test_f1():
     with unittest.mock.patch('builtins.input', return_value=1):
         a = [f(), f(), f()]
-        assert a[0] is a[1]
+        assert a[0] is a[1] and a[0] is a[2]
