e65c3be1083e86cabf73f858fb05d4a2616c7065
sweetysweat <aursegov000@gmail.com>
2021-09-22 00:25:52 +0300

    дз 5

--- homework5/task1.py
+++ homework5/task1.py
@@ -37,15 +37,25 @@ import datetime
 
 
 class Student:
-    def __init__(self,  first_name, last_name):
+    def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
+
+    @staticmethod
+    def do_homework(homework):
+        if homework.is_active():
+            return homework
+        return "You are late"
 
 
 class Teacher:
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
+
+    @staticmethod
+    def create_homework(homework_name, deadline):
+        return Homework(homework_name, deadline)
 
 
 class Homework:
@@ -54,27 +64,5 @@ class Homework:
         self.deadline = datetime.timedelta(days=deadline)
         self.created = datetime.datetime.now()
 
-
     def is_active(self):
-        self.
-
-# if __name__ == '__main__':
-#     teacher = Teacher('Daniil', 'Shadrin')
-#     student = Student('Roman', 'Petrov')
-#     # teacher.last_name  = Daniil
-#     # student.first_name  = Petrov
-#
-#     expired_homework = teacher.create_homework('Learn functions', 0)
-#     expired_homework.created  # Example: 2019-05-26 16:44:30.688762
-#     expired_homework.deadline  # 0:00:00
-#     expired_homework.text  # 'Learn functions'
-#
-#     # create function from method and use it
-#     create_homework_too = teacher.create_homework
-#     oop_homework = create_homework_too('create 2 simple classes', 5)
-#     oop_homework.deadline  # 5 days, 0:00:00
-#
-#     student.do_homework(oop_homework)
-#     student.do_homework(expired_homework)  # You are late
-
-
+        return self.created + self.deadline > datetime.datetime.now()
