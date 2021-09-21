ef4bc902ba3d2502781f3a45e5b7196aeaf3a6c2
sweetysweat <aursegov000@gmail.com>
2021-09-22 01:59:19 +0300

    еще раз

--- tests/homework1/test_3.py
+++ tests/homework1/test_3.py
@@ -7,6 +7,6 @@ from homework1.task3 import find_maximum_and_minimum
 path_to_test = os.getcwd() + "/tests/homework1/test_file.txt"
 
 
-@pytest.mark.parametrize("test_input, expectation", [['test_file.txt', (-1, 20)]])
+@pytest.mark.parametrize("test_input, expectation", [[path_to_test, (-1, 20)]])
 def test_find_max_and_min(test_input, expectation):
     assert find_maximum_and_minimum(test_input) == expectation
