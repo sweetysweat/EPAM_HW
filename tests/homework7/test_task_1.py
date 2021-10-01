113f5cfdb588209759f85d9b86b1708c52bc43fc
sweetysweat <aursegov000@gmail.com>
2021-10-01 13:47:41 +0300

    правки тестов

--- tests/homework7/test_task_1.py
+++ tests/homework7/test_task_1.py
@@ -27,10 +27,10 @@ def test_find_occurrences_red(test_input, element_to_find, expectation):
 
 
 @pytest.mark.parametrize("test_input, element_to_find, expectation", [[example_tree, "a", 1]])
-def test_find_occurrences_red(test_input, element_to_find, expectation):
+def test_find_occurrences_a(test_input, element_to_find, expectation):
     assert find_occurrences(test_input, element_to_find) == expectation
 
 
 @pytest.mark.parametrize("test_input, element_to_find, expectation", [[example_tree, "BLUE", 2]])
-def test_find_occurrences_red(test_input, element_to_find, expectation):
+def test_find_occurrences_blue(test_input, element_to_find, expectation):
     assert find_occurrences(test_input, element_to_find) == expectation
